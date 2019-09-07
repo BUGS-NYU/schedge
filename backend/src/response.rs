79ab9226feef853372ed74a81d172d897922476e
Albert Liu <albertymliu@gmail.com>
2019-09-07 19:17:26 -0400

    renaming

--- backend/src/response.rs
+++ backend/src/response.rs
@@ -1,5 +1,5 @@
 use crate::algorithm::compute_schedule;
-use crate::models::department::string_to_department_id;
+use crate::models::department::str_to_department_id;
 use crate::models::meeting::MeetingOutput;
 use rocket::http::Status;
 use rocket::response::status::Custom;
@@ -9,12 +9,12 @@ use rocket_contrib::json::Json;
 pub fn schedule_using_department(
     mut department: String,
 ) -> Result<Json<Vec<MeetingOutput>>, Custom<String>> {
-    let data = crate::seed::get_seed_data(); // TODO make this into a fairing
+    let data = crate::seed::get_seed_data(); // TODO make this into a stored state
                                              // TODO Move core out of seed data, into core seed data
 
     department.make_ascii_uppercase();
-    let expos_id = string_to_department_id("EXPOS-UA".into()).unwrap();
-    let major_dept_id = string_to_department_id(&department);
+    let expos_id = str_to_department_id("EXPOS-UA").unwrap();
+    let major_dept_id = str_to_department_id(&department);
     if let Some(major_id) = major_dept_id {
         let schedule = compute_schedule(&data, vec![expos_id, major_id]).unwrap_or(Vec::new());
         let schedule = schedule

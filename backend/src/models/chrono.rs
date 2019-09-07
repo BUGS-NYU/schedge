f2d3da63e9ad420a354109b67a9c696ed871339d
Albert Liu <albertymliu@gmail.com>
2019-09-07 03:47:33 -0400

    added chrono file for enums

---
+++ backend/src/models/chrono.rs
@@ -0,0 +1,38 @@
+use std::cmp::Ordering;
+
+/// Represents a day of the week
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Day {
+    Mon = 0,
+    Tues,
+    Wed,
+    Thurs,
+}
+
+impl Day {
+    pub fn as_num(self) -> u8 {
+        self as u8
+    }
+}
+
+/// Represents a time of day
+#[derive(Clone, Copy, Debug)]
+pub struct Time(pub u16);
+
+impl PartialEq for Time {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl PartialOrd for Time {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.0.partial_cmp(&other.0)
+    }
+}
+
+impl From<u16> for Time {
+    fn from(num: u16) -> Self {
+        Self(num)
+    }
+}

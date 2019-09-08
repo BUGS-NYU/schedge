4ca1f579da4038bd189e1e50207c63e84a7c2378
Albert Liu <albertymliu@gmail.com>
2019-09-07 21:05:56 -0400

    added rest of it lol

--- backend/src/seed.rs
+++ backend/src/seed.rs
@@ -1,17 +1,7 @@
 use crate::models::chrono::{Day, Time};
 use crate::models::course::Course;
-use crate::models::department::{CSCI_DEPT_ID, EXPOS_DEPT_ID, MATH_DEPT_ID};
+use crate::models::department::*;
 use crate::models::meeting::Meeting;
-
-// macro_rules! new_course {
-//     ($string:expr, $id:ident) => {
-//         Course {
-//             name: $string,
-//             department_id: $id,
-//             prerequisites: Vec::new(),
-//         }
-//     };
-// }
 
 /// Data is a vector of tuples where the first element is an identifier of the course,
 /// and the second is a list of meetings that that course contains.
@@ -55,23 +45,23 @@ pub fn get_seed_data() -> SeedData {
     }
 
     new_course!("Writing The Essay", EXPOS_DEPT_ID);
-    new_meeting!((Tues, Thurs), (1230, t1345), "Joseph Califf");
-    new_meeting!((Mon, Wed), (1230, t1345), "Noelle Liston");
-    new_meeting!((Tues, Thurs), (1100, t1215), "Matthew McClelland");
-    new_meeting!((Mon, Wed), (1100, t1215), "Noelle Liston");
+    new_meeting!((Tues, Thurs), (1230, 1345), "Joseph Califf");
+    new_meeting!((Mon, Wed), (1230, 1345), "Noelle Liston");
+    new_meeting!((Tues, Thurs), (1100, 1215), "Matthew McClelland");
+    new_meeting!((Mon, Wed), (1100, 1215), "Noelle Liston");
 
     new_course!("Introduction to Computer Science", CSCI_DEPT_ID);
-    new_meeting!((Tues, Thurs), (1400, t1515), "Anasse Bari");
-    new_meeting!((Tues, Thurs), (930, t1045), "Amos Bloomberg");
-    new_meeting!((Tues, Thurs), (1530, t1645), "Teseo Schneider");
-    new_meeting!((Mon, Wed), (1530, t1645), "Hilbert Locklear");
+    new_meeting!((Tues, Thurs), (1400, 1515), "Anasse Bari");
+    new_meeting!((Tues, Thurs), (930, 1045), "Amos Bloomberg");
+    new_meeting!((Tues, Thurs), (1530, 1645), "Teseo Schneider");
+    new_meeting!((Mon, Wed), (1530, 1645), "Hilbert Locklear");
 
     new_course!("Calculus I", MATH_DEPT_ID);
-    new_meeting!((Mon, Wed), (930, t1045), "Wayne Uy");
-    new_meeting!((Tues, Thurs), (1230, t1345), "Selin Kalaycioglu");
-    new_meeting!((Tues, Thurs), (1655, t1810), "N/A");
-    new_meeting!((Mon, Wed), (1400, t1515), "Sia Charmaine");
-    new_meeting!((Mon, Wed), (1230, t1345), "Hesam Oveys");
+    new_meeting!((Mon, Wed), (930, 1045), "Wayne Uy");
+    new_meeting!((Tues, Thurs), (1230, 1345), "Selin Kalaycioglu");
+    new_meeting!((Tues, Thurs), (1655, 1810), "N/A");
+    new_meeting!((Mon, Wed), (1400, 1515), "Sia Charmaine");
+    new_meeting!((Mon, Wed), (1230, 1345), "Hesam Oveys");
 
     new_course!(
         "Quantitative Reasoning: Elementary Statistics",

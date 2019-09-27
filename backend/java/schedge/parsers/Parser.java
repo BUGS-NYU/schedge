5cd3e80d24ef4bf5ffc87ff895fc6ed54e3834ff
Albert Liu <albertymliu@gmail.com>
2019-09-27 16:13:15 -0400

    refactored to fix errors

--- backend/java/schedge/parsers/Parser.java
+++ backend/java/schedge/parsers/Parser.java
@@ -1,20 +1,14 @@
 package schedge;
 
-import static schedge.wrapper.ConvenienceKt.*;
-
 import java.io.IOException;
-import java.lang.UnsupportedOperationException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import schedge.models.Course;
 import schedge.models.Section;
-import schedge.parsers.CourseParser;
+import parsers.CourseParser;
 import schedge.parsers.SectionParser;
 import schedge.wrapper.KtPair;
 
@@ -33,15 +27,13 @@ public class Parser {
     Elements elementList = xml.select(
         "div.primary-head ~ *"); // Get all siblings of the primary head
 
-    ArrayList<KtPair<Course, ArrayList<Section>>> output =
-        new ArrayList<KtPair<Course, ArrayList<Section>>>();
+    ArrayList<KtPair<Course, ArrayList<Section>>> output = new ArrayList<>();
 
     for (int i = 0; i < elementList.size(); i++) {
       Element element = elementList.get(i);
 
       if (!element.tagName().equals("div")) {
-        output.add(new KtPair<Course, ArrayList<Section>>(
-            parseCourseData(element), new ArrayList<Section>()));
+        output.add(new KtPair<>(parseCourseData(element), new ArrayList<>()));
       } else {
         // First element wasn't a `div` for some reason.
         assert output.isEmpty();

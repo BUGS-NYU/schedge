249752bf44a8f93163107c0bd231c4e70237b2b6
Albert Liu <albertymliu@gmail.com>
2019-10-05 02:09:23 -0400

    added correct database driver info, database dependency

--- backend/kotlin/database.kt
+++ backend/kotlin/database.kt
@@ -1,4 +1,13 @@
-// import org.jetbrains.exposed.sql.*
-// import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 
+
+fun hi() {
+    Database.connect(
+        "jdbc:postgresql://localhost:12346/test",
+        driver = "org.postgresql.Driver",
+        user = "user",
+        password = "password"
+    )
+}
 

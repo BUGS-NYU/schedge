c048c2996212c8cfea6e6db4efc0b986fa45465d
Albert Liu <albertymliu@gmail.com>
2019-10-04 22:42:07 -0400

    refactored build file, updated logging option

--- backend/kotlin/App.kt
+++ backend/kotlin/App.kt
@@ -1,21 +1,38 @@
-import java.io.IOException
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.groups.default
+import com.github.ajalt.clikt.parameters.groups.mutuallyExclusiveOptions
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.switch
 import com.github.ajalt.clikt.parameters.options.validate
 import com.github.ajalt.clikt.parameters.types.int
+import java.io.IOException
+
+fun validateLoggingLevel(level: Int?) {
+    val loggingLevel = level ?: Logging.WARN
+    Logging.loggingLevel = loggingLevel
+    val loggingLevelName = when (loggingLevel) {
+        1 -> "debug"
+        2 -> "info"
+        3 -> "warn"
+        4 -> "error"
+        else -> throw IOException("Logging level set to invalid value (${level}).")
+    }
+    Logging.info("Logging level set to $level (${loggingLevelName})")
+}
 
 class App() : CliktCommand() {
-    private val loggingLevel by option(help = "The logging level of the application.").int().default(Logging.WARN).validate {
-        Logging.loggingLevel = it
-        Logging.info("Logging level set to $it (${when (it) {
-            1 -> "debug"
-            2 -> "info"
-            3 -> "warn"
-            4 -> "error"
-            else -> throw IOException("Logging level set to invalid value.")
-        }})")
-    }
+    private val loggingLevel by mutuallyExclusiveOptions(
+        option(help="Set the logging level.")
+            .switch(
+                "--debug" to Logging.DEBUG,
+                "--info" to Logging.INFO,
+                "--warn" to Logging.WARN,
+                "--error" to Logging.ERROR
+            ).also { validateLoggingLevel(it.value) },
+        option("--loggingLevel", help="Set the logging level numerically.")
+            .int().also { validateLoggingLevel(it.value) }
+    )
 
     override fun run() = Unit
 }

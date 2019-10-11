47cbeca3616f8fe2bdadd207bafb3e7355e50105
Albert Liu <albertymliu@gmail.com>
2019-10-11 17:59:00 -0400

    tests aren't firing

--- backend/tests/scraping.kt
+++ backend/tests/scraping.kt
@@ -1,6 +1,14 @@
 import kotlin.test.Test
+import models.Schools
+import models.Subjects
+import models.Term
+import models.Semester.*
 import Scraper
+import Parser
 
-@Test fun hi() {
+@Test fun parseCatalog() {
+  val scraper = Scraper()
+  val parser = Parser()
+  val catalog_data = scraper.queryCourses(Term(Summer, 2019), Schools[0].abbrev, Subjects[0])
 }
 

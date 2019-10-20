c81b5ddb001dfbd67a3922651ee2cea0298f37eb
Albert Liu <albertymliu@gmail.com>
2019-10-20 02:57:04 -0400

    starting work on scraping CLI

--- kotlin/services/query_service.kt
+++ kotlin/services/query_service.kt
@@ -14,7 +14,6 @@ import org.apache.http.impl.client.HttpClients
 import org.apache.http.message.BasicNameValuePair
 import java.io.IOException
 
-
 fun queryCatalog(
     term: Term,
     school: School,
---
+++ kotlin/services/scrape_service.kt
@@ -0,0 +1,2 @@
+fun scrapeCatalog() {
+}

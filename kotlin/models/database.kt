6660277e685e00259ee19e00bdc7761d9318b742
Albert Liu <albertymliu@gmail.com>
2019-10-31 23:05:16 -0400

    refactored code a bit

--- kotlin/models/database.kt
+++ kotlin/models/database.kt
@@ -1,28 +1,34 @@
 package models
 
-//// import org.jetbrains.exposed.dao.*
-//
-//data class CourseAbbrev( // Gotten from catalog
-//    val name: String,
-//    val subject: String,
-//    val courseId: Int,
-//    val deptCourseNumber: Int // 7 in BIOL-UA 7
-//)
-//
-//data class SectionAbbrev( // Gotten from catalog
-//    val registrationNumber: Int,
-//    val type: SectionType
-//)
-//
-//data class Course( // Gotten from course page
-//    val name: String
-//)
-//
-//data class Section( // Gotten from course page
-//    val course: Course,
-//    val type: SectionType,
-//    val status: SectionStatus,
-//    val meetings: List<Meeting>,
-//    val associatedWith: Section?
-//)
+class Course {
+
+    val name: String
+    val deptCourseNumber: Int
+    val sections: Array<Section>
+
+    companion object {
+        fun getCourses(subject: Subject, term: Term): Array<Course> {
+            return arrayOf()
+        }
+    }
+
+    private constructor(
+        name: String,
+        deptCourseNumber: Int,
+        sections: Array<Section>
+    ) {
+        this.name = name
+        this.deptCourseNumber = deptCourseNumber
+        this.sections = sections
+    }
+}
+
+class Section(
+    val registrationNumber: Int,
+    val sectionNumber: Int,
+    val instructor: String,
+    val type: SectionType,
+    val associatedWith: Section?,
+    val meetings: Array<Meeting>
+)
 

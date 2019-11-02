f5dcdb0aa92543ddac2a313024ae19443ec2b5fd
Albert Liu <albertymliu@gmail.com>
2019-11-01 22:19:25 -0400

    added TODO, small refactor

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# ToDo
+- Write code for `schedge scrape catalog`
+- Write help messages
+- Write web API
+  - Deploy
--- kotlin/cli/Schedge.kt
+++ kotlin/cli/Schedge.kt
@@ -10,18 +10,7 @@ internal class Schedge() : CliktCommand(name = "schedge") {
         this.subcommands(Query(), Parse(), Database())
     }
 
-    override fun run() {
-        // connectToDatabase(logger)
-        // transaction {
-        //     // TODO Handle migrations instead of just adding missing tables and columns
-        //     SchemaUtils.createMissingTablesAndColumns(Migrations)
-        //     SchemaUtils.createMissingTablesAndColumns(*Tables)
-        // }
-
-        // println(models.Subjects)
-        // println(models.Schools)
-        // logger.error("Nothing's been implemented!")
-    }
+    override fun run() = Unit
 }
 
 

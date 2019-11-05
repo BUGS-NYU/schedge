3ab6f923c9abfd174b162fb76a8b7342447a79a4
Albert Liu <albertymliu@gmail.com>
2019-11-05 00:56:17 -0500

    reformatted

--- java/api/App.java
+++ java/api/App.java
@@ -5,13 +5,12 @@ import models.Subject;
 import models.Term;
 
 public class App {
-    public static void run() {
-        Javalin app = Javalin.create().start(80);
-        app.get("/:term/:school/:subject", ctx -> {
-            Term term = Term.fromId(Integer.parseInt(ctx.pathParam("term")));
-            Subject subject =
-                    new Subject(ctx.pathParam("subject"), ctx.pathParam("school"));
-
-        });
-    }
+  public static void run() {
+    Javalin app = Javalin.create().start(80);
+    app.get("/:term/:school/:subject", ctx -> {
+      Term term = Term.fromId(Integer.parseInt(ctx.pathParam("term")));
+      Subject subject =
+          new Subject(ctx.pathParam("subject"), ctx.pathParam("school"));
+    });
+  }
 }
--- java/api/Yacs.java
+++ java/api/Yacs.java
@@ -1,2 +1,1 @@
-public class Yacs {
-}
+public class Yacs {}

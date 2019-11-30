fc5e5177478bc504cd9a9c7aca7f7f998ffbd44d
Albert Liu <albertymliu@gmail.com>
2019-11-30 00:06:57 -0500

    added beginnings of insert courses service

---
+++ java/services/InsertCourses.java
@@ -0,0 +1,10 @@
+package services;
+
+import models.*;
+import org.slf4j.Logger;
+import java.util.List;
+
+public final class InsertCourses {
+
+  public static void insert(List<Course> courses) {}
+}

67fad9d9718ccd9311a317751590f41c2eab1053
Albert Liu <albertymliu@gmail.com>
2019-11-30 01:48:51 -0500

    added beginnings of jooq stuff

--- java/services/InsertCourses.java
+++ java/services/InsertCourses.java
@@ -1,6 +1,9 @@
 package services;
 
 import models.*;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 import org.slf4j.Logger;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -9,7 +12,7 @@ import java.util.List;
 public class InsertCourses {
   public static void insertCourses(Logger logger, List<Course> courses) {
     try (Connection conn = GetConnection.getConnection()) {
-
+        DSLContext create = DSL.using(conn, SQLDialect.POSTGRES);
     } catch (SQLException e) {
     }
   }

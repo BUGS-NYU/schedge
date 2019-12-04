c9779ecb3f342522bcbcb39a685719c7ddafbeef
Albert Liu <albertymliu@gmail.com>
2019-12-04 01:46:03 -0500

    added async version of query_catalog code

--- src/main/kotlin/services/query_catalog.kt
+++ src/main/kotlin/services/query_catalog.kt
@@ -1,53 +1,57 @@
-    package services
+package services
 
+import com.github.kittinunf.fuel.Fuel
 import models.SubjectCode
 import models.Term
 import mu.KLogger
 import mu.KotlinLogging
-import org.apache.http.client.entity.UrlEncodedFormEntity
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.client.methods.HttpUriRequest
-import org.apache.http.client.protocol.HttpClientContext
-import org.apache.http.impl.client.BasicCookieStore
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.message.BasicNameValuePair
 import java.io.IOException
+import java.net.HttpCookie
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Future
+import kotlin.math.max
+import kotlin.math.min
+
+val queryLogger = KotlinLogging.logger("services.query_catalog");
+private const val ROOT_URL = "https://m.albert.nyu.edu/app/catalog/classSearch"
+private const val DATA_URL = "https://m.albert.nyu.edu/app/catalog/getClassSearch"
+
 /**
-Make Query Service
+ * Query the catalog for data
  */
-fun queryCatalog(logger: KLogger, term: Term, subjectCode: SubjectCode): String = queryCatalog(logger, term, subjectCode, AlbertClient())
+fun queryCatalog(logger: KLogger, term: Term, subjectCode: SubjectCode): String {
+    return queryCatalog(term, subjectCode, getToken()).get()
+        ?: throw IOException("No classes found matching criteria school=${subjectCode.school}, subject=${subjectCode.abbrev}")
+}
+
 /**
-Querying the catalog given one subject code.
-@param Logger
-@param Term (class)
-@param SubjectCode (class)
-@param AlbertClient (class)
-@return String
+ * The meat of querying the catalog resides here.
  */
-private fun queryCatalog(logger: KLogger, term: Term, subjectCode: SubjectCode, client: AlbertClient): String {
-    logger.info { "querying catalog for term=$term and subject=$subjectCode..." }
-    val params = mutableListOf( // URL params
-        BasicNameValuePair("CSRFToken", client.csrfToken),
-        BasicNameValuePair("term", term.id.toString()),
-        BasicNameValuePair("acad_group", subjectCode.school),
-        BasicNameValuePair("subject", subjectCode.abbrev)
+private fun queryCatalog(term: Term, subjectCode: SubjectCode, csrfToken: String): Future<String?> {
+    queryLogger.info { "querying catalog for term=$term and subject=$subjectCode..." }
+    val params = listOf( // URL params
+        "CSRFToken" to csrfToken,
+        "term" to term.id.toString(),
+        "acad_group" to subjectCode.school,
+        "subject" to subjectCode.abbrev
     )
-    logger.debug { "Params are ${params}." }
 
-    
-    val request = HttpPost(DATA_URL).apply {
-        entity = UrlEncodedFormEntity(params)
-        addHeader("Referrer", "${ROOT_URL}/${term.id}")
-        addHeader("Host", "m.albert.nyu.edu")
-    }
+    queryLogger.debug { "Params are ${params}." }
+    val future = CompletableFuture<String?>()
 
-    val result = client.execute(request)
-    return if (result == "No classes found matching your criteria.") {
-        throw IOException("No classes found matching criteria school=${subjectCode.school}, subject=${subjectCode.abbrev}")
-    } else {
-        result
+    Fuel.post(DATA_URL, params).set("Referrer", "${ROOT_URL}/${term.id}").set(
+        "Host",
+        "m.albert.nyu.edu"
+    ).response { _, response, _ ->
+        val result = String(response.data)
+        if (result == "No classes found matching your criteria.") {
+            queryLogger.warn { "No classes found matching criteria school=${subjectCode.school}, subject=${subjectCode.abbrev}" }
+            future.complete(null);
+        } else {
+            future.complete(result)
+        }
     }
+    return future
 }
 
 /**
@@ -59,66 +63,98 @@ Querying Catalog given list of subject codes
  */
 fun queryCatalog(logger: KLogger, term: Term, subjectCodes: List<SubjectCode>): Sequence<String> {
     if (subjectCodes.size > 1) {
-        logger.info { "querying catalog for term=$term with multiple subjects..." }
+        queryLogger.info { "querying catalog for term=$term with multiple subjects..." }
     }
-    val client = AlbertClient()
-
-    return subjectCodes.asSequence().map { subject ->
-        queryCatalog(logger, term, subject, client)
-    }
+    return QueryResults(term, subjectCodes, max(5, min(subjectCodes.size / 5, 20))).asSequence()
 }
 
-private const val ROOT_URL = "https://m.albert.nyu.edu/app/catalog/classSearch"
-private const val DATA_URL = "https://m.albert.nyu.edu/app/catalog/getClassSearch"
 /**
-Albert Client class to handle HTTP Request
+ * This class tries to emulate batch processing, with multiple requests potentially in flight at the same time.
+ *
+ * Internally, it maintains a set of mailboxes which it iterates over, waiting on each one for new data to arrive.
+ * While it waits, other "mail" might be arriving in other mailboxes.
  */
-private class AlbertClient {
-    private val logger = KotlinLogging.logger {}
-    private val httpClient = HttpClients.custom().useSystemProperties().build()
-    private val httpContext = HttpClientContext.create().apply {
-        cookieStore = BasicCookieStore()
-    }
-    /**
-     * Retrieve the csrfToken for making HTTP request to Albert Mobile
-     */
-    val csrfToken: String
+private class QueryResults(val term: Term, val subjects: List<SubjectCode>, arraySize: Int) :
+    Iterator<String> {
 
     init {
-        logger.debug("Creating client instance...")
+        require(arraySize > 0) { "Need to have a non-empty array size!" }
+        queryLogger.info { "Array size is $arraySize" }
+    }
 
-        // Get a CSRF token for this client. This token allows us to get
-        // data straight from NYU's internal web APIs.
-        val response = httpClient.execute(HttpGet(ROOT_URL), httpContext)
-        response.close()
-        val cookie = httpContext.cookieStore.cookies.find { cookie ->
-                cookie.name == "CSRFCookie"
-            }
+    var subjectsIndex = min(subjects.size, arraySize)
+    var pendingRequests = subjectsIndex
+    var arrayIndex = 0
+    val tokens = Array(subjectsIndex) {
+        getTokenAsync()
+    }.map {
+        it.get()
+    }
+    val requests = Array(subjectsIndex) {
+        queryCatalog(term, subjects[it], tokens[it])
+    }
+    var result: String? = null
 
-        if (cookie == null) {
-            logger.error {
-                val cookies = httpContext.cookieStore.cookies.map {
-                    "${it.name}: \"${it.value}\""
-                }
-                "Couldn't find `CSRFCookie`. " +
-                        "Cookies found were [\n  ${cookies.joinToString(",\n  ")}]."
+    init {
+        queryLogger.info { "subjectsIndex is $subjectsIndex" }
+        result = tryGetNext()
+    }
+
+    private fun tryGetNext(): String? {
+        var fetchedResult: String? = null
+        while (fetchedResult == null && pendingRequests > 0) {
+            fetchedResult = requests[arrayIndex].get()
+            queryLogger.info { "fetchedResult is $fetchedResult" }
+            if (subjectsIndex < subjects.size) {
+                requests[arrayIndex] = queryCatalog(term, subjects[subjectsIndex], tokens[arrayIndex])
+                subjectsIndex++
+            } else {
+                pendingRequests--
             }
-            throw IOException("NYU servers did something unexpected.")
-        } else {
-            csrfToken = cookie.value
+            arrayIndex++
+            if (arrayIndex == tokens.size) arrayIndex = 0
         }
 
-        logger.info { "Client instance created with CSRF Token '${csrfToken}'." }
+        return fetchedResult
     }
 
-    /**
-    Making the HTTPRequest to Albert Mobile
-    @param HTTPUriRequest
-    @return String
-     */
-    fun execute(req: HttpUriRequest): String {
-        logger.trace { "Executing ${req.method.toUpperCase()} request" }
-        return this.httpClient.execute(req, httpContext).entity.content.bufferedReader().readText()
+    override fun hasNext(): Boolean {
+        return result != null
     }
+
+    override fun next(): String {
+        if (result == null) throw NoSuchElementException()
+        val cachedResult = this.result
+        this.result = tryGetNext()
+        return cachedResult!!
+    }
+
+}
+
+/**
+ * Get a CSRF Token from NYU
+ */
+fun getToken(): String = getTokenAsync().get()
+
+fun getTokenAsync(): Future<String> {
+    val future = CompletableFuture<String>()
+
+    queryLogger.info { "Getting CSRF token..." }
+    Fuel.get(ROOT_URL).response { _, response, _ ->
+        val token = response.headers["Set-Cookie"].flatMap {
+            HttpCookie.parse(it)!!
+        }.find {
+            it.name == "CSRFCookie"
+        }?.value
+
+        if (token == null) {
+            queryLogger.error("Couldn't find cookie with name=CSRFCookie")
+            throw IOException("NYU servers did something unexpected.")
+        }
+
+        queryLogger.info { "Retrieved CSRF token `${token}`" }
+        future.complete(token)
+    }
+    return future
 }
 

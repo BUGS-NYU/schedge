a3c497a769013d5222ea5871adb46d1fc48fb3c7
[Kn99HN] <khanhnguyen99hn@gmail.com>
2020-01-28 18:17:39 -0500

    reformat with clang

--- src/main/java/cli/parse.java
+++ src/main/java/cli/parse.java
@@ -18,39 +18,40 @@ import utils.UtilsKt;
    @Todo: Add annotation for parameter. Fix the method to parse
    @Help: Add annotations, comments to code
 */
-@CommandLine.Command(
-        name = "parse", synopsisSubcommandLabel = "(catalog | section | school)",
-        subcommands = {parse.Catalog.class, parse.Section.class, parse.School.class})
+@CommandLine.Command(name = "parse",
+                     synopsisSubcommandLabel = "(catalog | section | school)",
+                     subcommands = {parse.Catalog.class, parse.Section.class,
+                                    parse.School.class})
 public class parse implements Runnable {
-  @CommandLine.Spec
-  private CommandLine.Model.CommandSpec spec;
+  @CommandLine.Spec private CommandLine.Model.CommandSpec spec;
 
   @Override
   public void run() {
     throw new CommandLine.ParameterException(spec.commandLine(),
-            "Missing required subcommand");
+                                             "Missing required subcommand");
   }
 
-
-  @CommandLine.Command(
-          name = "section",
-          sortOptions = false,
-          headerHeading = "Usage:%n%n",
-          synopsisHeading = "%n",
-          descriptionHeading = "%nDescription:%n%n",
+  @CommandLine.
+  Command(name = "section", sortOptions = false, headerHeading = "Usage:%n%n",
+          synopsisHeading = "%n", descriptionHeading = "%nDescription:%n%n",
           parameterListHeading = "%nParameters:%n",
-          optionListHeading = "%nOptions:%n",
-          header = "Parse catalog",
-          description = "Parse catalog based on term, subject codes, " +
-                  "or school for one or multiple subjects/schools")
+          optionListHeading = "%nOptions:%n", header = "Parse catalog",
+          description = "Parse catalog based on term, subject codes, "
+                        + "or school for one or multiple subjects/schools")
   public static class Section implements Runnable {
     private Logger logger = LoggerFactory.getLogger("parse.section");
 
     @CommandLine.Option(names = "--pretty", defaultValue = "false")
     private String pretty;
-    @CommandLine.Option(names = "--input-file", description = "intput file to read from. If none provided, read from stdout")
+    @CommandLine.
+    Option(names = "--input-file",
+           description =
+               "intput file to read from. If none provided, read from stdout")
     private String inputFile;
-    @CommandLine.Option(names = "--output-file", description = "output file to write to. If none provided, write to stdout")
+    @CommandLine.
+    Option(names = "--output-file",
+           description =
+               "output file to write to. If none provided, write to stdout")
     private String outputFile;
 
     @Override
@@ -60,8 +61,8 @@ public class parse implements Runnable {
         String input = UtilsKt.readFromFileOrStdin(inputFile);
         SectionAttribute output = ParseSection.parse(input);
         UtilsKt.writeToFileOrStdout(
-                outputFile,
-                JsonMapper.toJson(output, Boolean.parseBoolean(pretty)));
+            outputFile,
+            JsonMapper.toJson(output, Boolean.parseBoolean(pretty)));
         long end = System.nanoTime();
         long duration = (end - start) / 1000000000;
         logger.info(duration + " seconds");
@@ -72,23 +73,26 @@ public class parse implements Runnable {
   }
 
   @CommandLine.Command(
-          name = "catalog",
-          sortOptions = false,
-          headerHeading = "Usage:%n%n",
-          synopsisHeading = "%n",
-          descriptionHeading = "%nDescription:%n%n",
-          parameterListHeading = "%nParameters:%n",
-          optionListHeading = "%nOptions:%n",
-          header = "Parse catalog",
-          description = "Parse catalog based on input file. If not provided, read from stdin")
+      name = "catalog", sortOptions = false, headerHeading = "Usage:%n%n",
+      synopsisHeading = "%n", descriptionHeading = "%nDescription:%n%n",
+      parameterListHeading = "%nParameters:%n",
+      optionListHeading = "%nOptions:%n", header = "Parse catalog",
+      description =
+          "Parse catalog based on input file. If not provided, read from stdin")
   public static class Catalog implements Runnable {
     private Logger logger = LoggerFactory.getLogger("parse.catalog");
 
     @CommandLine.Option(names = "--pretty", defaultValue = "false")
     private String pretty;
-    @CommandLine.Option(names = "--input-file", description = "intput file to read from. If none provided, read from stdout")
+    @CommandLine.
+    Option(names = "--input-file",
+           description =
+               "intput file to read from. If none provided, read from stdout")
     private String inputFile;
-    @CommandLine.Option(names = "--output-file", description = "output file to write to. If none provided, write to stdout")
+    @CommandLine.
+    Option(names = "--output-file",
+           description =
+               "output file to write to. If none provided, write to stdout")
     private String outputFile;
 
     @Override
@@ -98,8 +102,8 @@ public class parse implements Runnable {
         String input = UtilsKt.readFromFileOrStdin(inputFile);
         List<Course> output = ParseCatalog.parse(input, null);
         UtilsKt.writeToFileOrStdout(
-                JsonMapper.toJson(output, Boolean.parseBoolean(pretty)),
-                outputFile);
+            JsonMapper.toJson(output, Boolean.parseBoolean(pretty)),
+            outputFile);
         long end = System.nanoTime();
         long duration = (end - start) / 1000000000;
         logger.info(duration + " seconds");
@@ -109,36 +113,37 @@ public class parse implements Runnable {
     }
   }
 
-
   // @ToDo: Fix this with two options
-  @CommandLine.Command(
-          name = "school",
-          sortOptions = false,
-          headerHeading = "Usage:%n%n",
-          synopsisHeading = "%n",
-          descriptionHeading = "%nDescription:%n%n",
+  @CommandLine.
+  Command(name = "school", sortOptions = false, headerHeading = "Usage:%n%n",
+          synopsisHeading = "%n", descriptionHeading = "%nDescription:%n%n",
           parameterListHeading = "%nParameters:%n",
-          optionListHeading = "%nOptions:%n",
-          header = "Parse school/subject",
+          optionListHeading = "%nOptions:%n", header = "Parse school/subject",
           description = "Parse school/subject based on input file")
   public static class School implements Runnable {
     private Logger logger = LoggerFactory.getLogger("parse.catalog");
 
     @CommandLine.Option(
-            names = "--school",
-            description =
-                    "Enter no if not want. If none provided, will read the school values")
+        names = "--school",
+        description =
+            "Enter no if not want. If none provided, will read the school values")
     private String school;
     @CommandLine.Option(
-            names = "--subject",
-            description =
-                    "Enter no if not want. If none provided, will read the subject values")
+        names = "--subject",
+        description =
+            "Enter no if not want. If none provided, will read the subject values")
     private String subject;
     @CommandLine.Option(names = "--pretty", defaultValue = "false")
     private String pretty;
-    @CommandLine.Option(names = "--input-file", description = "intput file to read from. If none provided, read from stdout")
+    @CommandLine.
+    Option(names = "--input-file",
+           description =
+               "intput file to read from. If none provided, read from stdout")
     private String inputFile;
-    @CommandLine.Option(names = "--output-file", description = "output file to write to. If none provided, write to stdout")
+    @CommandLine.
+    Option(names = "--output-file",
+           description =
+               "output file to write to. If none provided, write to stdout")
     private String outputFile;
 
     @Override
@@ -146,26 +151,28 @@ public class parse implements Runnable {
       try {
         long start = System.nanoTime();
         String input = UtilsKt.readFromFileOrStdin(inputFile);
-        if(school == null && subject == null) {
+        if (school == null && subject == null) {
           Map<String, String> schools = ParseSchoolSubjects.parseSchool(input);
-          Map<String, String> subjects = ParseSchoolSubjects.parseSubject(input);
+          Map<String, String> subjects =
+              ParseSchoolSubjects.parseSubject(input);
           UtilsKt.writeToFileOrStdout(
-                  JsonMapper.toJson(schools, Boolean.parseBoolean(pretty)),
-                  outputFile);
+              JsonMapper.toJson(schools, Boolean.parseBoolean(pretty)),
+              outputFile);
 
           UtilsKt.writeToFileOrStdout(
-                  JsonMapper.toJson(subjects, Boolean.parseBoolean(pretty)),
-                  outputFile);
-        } else if(subject == null) {
-          Map<String, String> subjects = ParseSchoolSubjects.parseSubject(input);
+              JsonMapper.toJson(subjects, Boolean.parseBoolean(pretty)),
+              outputFile);
+        } else if (subject == null) {
+          Map<String, String> subjects =
+              ParseSchoolSubjects.parseSubject(input);
           UtilsKt.writeToFileOrStdout(
-                  JsonMapper.toJson(subjects, Boolean.parseBoolean(pretty)),
-                  outputFile);
+              JsonMapper.toJson(subjects, Boolean.parseBoolean(pretty)),
+              outputFile);
         } else {
           Map<String, String> schools = ParseSchoolSubjects.parseSchool(input);
           UtilsKt.writeToFileOrStdout(
-                  JsonMapper.toJson(schools, Boolean.parseBoolean(pretty)),
-                  outputFile);
+              JsonMapper.toJson(schools, Boolean.parseBoolean(pretty)),
+              outputFile);
         }
         long end = System.nanoTime();
         long duration = (end - start) / 1000000000;

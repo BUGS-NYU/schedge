5a1094e50e17387b4fb2834d5e599a6ae79db0e2
Albert Liu <albertymliu@gmail.com>
2020-02-26 17:13:55 -0500

    reformatting

--- src/main/kotlin/services/master_scraper.kt
+++ src/main/kotlin/services/master_scraper.kt
@@ -12,62 +12,79 @@ import kotlin.math.min
 import kotlin.streams.asStream
 import kotlin.streams.toList
 
-fun masterScrapeSection(term : Term, subjectCode: SubjectCode): List<Course> {
+fun masterScrapeSection(term: Term, subjectCode: SubjectCode): List<Course> {
     val courses = scrapeFromCatalog(term, subjectCode)
-    val iterator = courses.stream().map {
-        course -> course.sections
-    }.flatMap {
-        mutableList -> mutableList.stream()
+    val iterator = courses.stream().map { course ->
+        course.sections
+    }.flatMap { mutableList ->
+        mutableList.stream()
     }.iterator()
 
-    val size = courses.map {
-        course -> course.sections
+    val size = courses.map { course ->
+        course.sections
     }.flatten().toList().size
 
     SimpleBatchedFutureEngine<Section, Void>(
             iterator, size
-    ){
-        section, _ -> section.update(querySection(term, section.registrationNumber))
+    ) { section, _ ->
+        section.update(querySection(term, section.registrationNumber))
     }
 
     return courses
 }
 
-fun masterScrapeSection(term : Term, forSchool: String, batchSizeNullable: Int?) : Sequence<List<Course>>{
-    val courses = scrapeFromCatalog(term, SubjectCode.allSubjects(forSchool),batchSizeNullable)
-    val iterator = courses.flatten().asStream().map {
-        course -> course.sections
-    }.flatMap {
-        mutableList -> mutableList.stream()
+fun masterScrapeSection(term: Term, forSchool: String, batchSizeNullable: Int?): Sequence<List<Course>> {
+    val courses = scrapeFromCatalog(term, SubjectCode.allSubjects(forSchool), batchSizeNullable)
+    val iterator = courses.flatten().asStream().map { course ->
+        course.sections
+    }.flatMap { mutableList ->
+        mutableList.stream()
     }.iterator()
 
-    val size = courses.flatten().map {
-        course -> course.sections
+    val size = courses.flatten().map { course ->
+        course.sections
     }.flatten().toList().size
     SimpleBatchedFutureEngine<Section, Void>(
             iterator, size
-    ){
-        section, _ -> section.update(querySection(term, section.registrationNumber))
+    ) { section, _ ->
+        section.update(querySection(term, section.registrationNumber))
     }
     return courses
 }
 
 
-fun masterScrapeSection(term : Term, subjectCodes: List<SubjectCode>, batchSizeNullable: Int?) : Sequence<List<Course>> {
+fun masterScrapeSection(term: Term, subjectCodes: List<SubjectCode>, batchSizeNullable: Int?): Sequence<List<Course>> {
     val courses = scrapeFromCatalog(term, subjectCodes, batchSizeNullable)
-    val iterator = courses.flatten().asStream().map {
-        course -> course.sections
-    }.flatMap {
-        mutableList -> mutableList.stream()
+    val iterator = courses.flatten().asStream().map { course ->
+        course.sections
+    }.flatMap { mutableList ->
+        mutableList.stream()
     }.iterator()
 
-    val size = courses.flatten().map {
-        course -> course.sections
+    val size = courses.flatten().map { course ->
+        course.sections
     }.flatten().toList().size
     SimpleBatchedFutureEngine<Section, Void>(
-            iterator,size
-    ){
-        section, _ -> section.update(querySection(term, section.registrationNumber))
+            iterator, size
+    ) { section, _ ->
+        section.update(querySection(term, section.registrationNumber))
     }
     return courses
+}
+
+private fun scrapeCourseSections(term: Term, courses: Sequence<Course>, batchSizeNullable: Int? = null): List<Course> {
+    val courseList: MutableList<Course> = mutableListOf()
+    val iterator = courses.map { course ->
+        courseList.add(course)
+        course.sections
+    }.flatMap { mutableList ->
+        mutableList.asSequence()
+    }.iterator()
+
+    val batchSize = batchSizeNullable ?: 20 // @Performance What should this number be?
+
+    SimpleBatchedFutureEngine(iterator, batchSize) { section, _ ->
+        section.update(querySection(term, section.registrationNumber))
+    }.forEach { _ -> }
+    return courseList
 }

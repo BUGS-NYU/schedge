b93d1077285383a1fa2fd1c86a37c86bf0407f9a
Albert Liu <albertymliu@gmail.com>
2020-03-05 17:48:37 -0500

    added jks support

--- recert.sh
+++ recert.sh
@@ -3,14 +3,13 @@
 # https://www.wissel.net/blog/2018/03/letsencrypt-java-keystore.html
 
 sudo certbot renew
-
 cat /etc/letsencrypt/live/schedge.torchnyu.com/*.pem > fullcert.pem
-keytool -genkey -keyalg RSA -alias keystore -keystore empty.jks
-keytool -delete -alias sfdcsec -keystore empty.jks
-cp empty.jks keystore.jks
+openssl pkcs12 -export -out fullcert.pkcs12 -in fullcert.pem
+rm fullcert.pem
+cp src/main/resources/empty.jks keystore.jks
 keytool -v -importkeystore -srckeystore fullcert.pkcs12 -destkeystore keystore.jks -deststoretype JKS
-
-cp keystore.jks src/main/resources
+rm fullcert.pkcs12
+mv keystore.jks src/main/resources
 
 ./gradlew build
 ./schedge db serve

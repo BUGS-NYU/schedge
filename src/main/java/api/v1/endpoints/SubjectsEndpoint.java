18f3eb6db9f01769eb42ab3218bdd42eaebc63f2
Albert Liu <albertymliu@gmail.com>
2020-03-08 23:00:36 -0400

    altered documentation

--- src/main/java/api/v1/endpoints/SubjectsEndpoint.java
+++ src/main/java/api/v1/endpoints/SubjectsEndpoint.java
@@ -20,42 +20,19 @@ public final class SubjectsEndpoint extends Endpoint {
   public OpenApiDocumentation configureDocs(OpenApiDocumentation docs) {
     return docs
         .operation(openApiOperation -> {
-          // openApiOperation.operationId("Operation Id");
           openApiOperation.description(
-              "This endpoint returns a list of subjects, optionally filtered by a school.");
+              "This endpoint returns an object whose keys are schools, and whose "
+            + "values are objects with subject codes as keys and subject names as values.");
           openApiOperation.summary("Subjects Endpoint");
-        })
-        .queryParam(
-            "school", String.class,
-            openApiParam -> {
-              openApiParam.description(
-                  "The school code you'd like to get subjects for (case insensitive). "
-                  +
-                  "If not supplied, then this endpoint will return all subjects.");
-            })
-        .json("400", ApiError.class,
-              openApiParam -> {
-                openApiParam.description(
-                    "The school provided was not a valid school.");
-              })
-        .jsonArray("200", SubjectCode.SubjectMetadata.class,
-                   openApiParam -> { openApiParam.description("OK."); });
+        });
   }
 
   @NotNull
   @Override
   public Handler getHandler() {
     return ctx -> {
-      String school = ctx.queryParam("school");
       ctx.contentType("application/json");
-      if (school == null) {
-        ctx.json(SubjectCode.getAvailableSubjectInfo());
-      } else
-        try {
-          ctx.json(SubjectCode.allSubjectInfoForSchool(school.toUpperCase()));
-        } catch (IllegalArgumentException e) {
-          ctx.json(new ApiError(e.getMessage()));
-        }
+      ctx.json(SubjectCode.getAvailableSubjectInfo());
     };
   }
 }

29d346e66206d0a51dd3cd72c6e6f42187325435
Albert Liu <albertymliu@gmail.com>
2020-05-06 15:15:15 -0400

    added setup stuff for server

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install nginx
+
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt-get update
+sudo apt-get install oracle-java14-installer
+
+sudo apt install python-certbot-nginx
+sudo certbot --nginx -d schedge.a1liu.com
+

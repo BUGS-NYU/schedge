c41bcea201d47cc8830f142d525a8f3cb62ec992
Albert Liu <albertymliu@gmail.com>
2020-06-04 17:57:35 -0400

    fixed bugs, added limit to search again

--- src/main/java/database/epochs/LatestCompleteEpoch.java
+++ src/main/java/database/epochs/LatestCompleteEpoch.java
@@ -15,17 +15,16 @@ public final class LatestCompleteEpoch {
       throws SQLException {
     PreparedStatement stmt = conn.prepareStatement(
         "SELECT max(id) from epochs WHERE completed_at IS NOT NULL "
-        + "AND term_id = ? ORDER BY completed_at DESC LIMIT 1");
+        + "AND term_id = ? LIMIT 1");
 
     try (ResultSet rs = Utils.setArray(stmt, term.getId()).executeQuery()) {
-      int e = rs.getInt(1);
-      if (rs.wasNull()) {
+      if (!rs.next()) {
         logger.info("Couldn't find epoch for term=" + term);
         return null;
-      } else {
-        logger.info("found epoch=" + e + " for term=" + term);
-        return e;
       }
+      int e = rs.getInt(1);
+      logger.info("found epoch=" + e + " for term=" + term);
+      return e;
     }
   }
 }

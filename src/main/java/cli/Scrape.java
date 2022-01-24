0ffc8ea551acec1528c42f2bdd35f1251e2ab9ea
Albert Liu <albertymliu@gmail.com>
2022-01-24 06:24:53 -0500

    idk man

--- src/main/java/cli/Scrape.java
+++ src/main/java/cli/Scrape.java
@@ -4,6 +4,9 @@ import cli.templates.OutputFileMixin;
 import cli.templates.RegistrationNumberMixin;
 import cli.templates.SubjectCodeMixin;
 import cli.templates.TermMixin;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 import nyu.SubjectCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,10 +16,6 @@ import scraping.ScrapeSection;
 import scraping.parse.ParseSchoolSubjects;
 import scraping.query.QuerySchool;
 
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
-
 /*
    @Todo: Add annotation for parameter.
 */
@@ -24,7 +23,7 @@ import java.util.stream.Collectors;
 Command(name = "scrape",
         description =
             "Query then parse NYU Albert data based on different catagories",
-        synopsisSubcommandLabel = "(catalog | sections | school)")
+        synopsisSubcommandLabel = "(catalog | sections | school | schedge)")
 public class Scrape implements Runnable {
   @CommandLine.Spec private CommandLine.Model.CommandSpec spec;
 
@@ -39,6 +38,28 @@ public class Scrape implements Runnable {
         spec.commandLine(),
         "\nMissing required subcommand. Try ./schedge scrape [subcommand] --help to"
             + " display help message for possible subcommands");
+  }
+
+  @CommandLine.
+  Command(name = "schedge", sortOptions = false, headerHeading = "Command: ",
+          descriptionHeading = "%nDescription:%n%n",
+          parameterListHeading = "%nParameters:%n",
+          optionListHeading = "%nOptions:%n", header = "Scrape section",
+          description = "Scrape existing Schedge instance")
+  public void
+  schedge(
+      @CommandLine.
+      Option(names = "--domain", defaultValue = "schedge.a1liu.com",
+             description = "domain to scrape as if it's an instance of schedge")
+      String domain,
+      @CommandLine.Mixin OutputFileMixin outputFileMixin) {
+    // TODO this will eventually scrape directly from the new API  instead of
+    // the old one
+    long start = System.nanoTime();
+
+    long end = System.nanoTime();
+    double duration = (end - start) / 1000000000.0;
+    logger.info(duration + "seconds");
   }
 
   @CommandLine.Command(

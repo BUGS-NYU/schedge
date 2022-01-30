f27cb7525d94f408d50131a284a32992437112d3
Albert Liu <albertymliu@gmail.com>
2022-01-30 16:49:00 -0500

    simplifying piece by piece

--- src/main/java/scraping/query/GetClient.java
+++ src/main/java/scraping/query/GetClient.java
@@ -2,20 +2,27 @@ package scraping.query;
 
 import static utils.TryCatch.*;
 
+import io.netty.handler.codec.http.cookie.Cookie;
 import java.io.IOException;
+import java.util.*;
+import java.util.function.Predicate;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClientConfig;
+import org.asynchttpclient.cookie.CookieStore;
+import org.asynchttpclient.uri.Uri;
 
 public final class GetClient {
   private static volatile AsyncHttpClient client;
 
-  public static AsyncHttpClient getClient() {
-    if (client == null)
+  public static synchronized AsyncHttpClient getClient() {
+    if (client == null) {
       client = new DefaultAsyncHttpClient(
           new DefaultAsyncHttpClientConfig.Builder()
-              .setCookieStore(BlackholeCookieStore.BLACK_HOLE)
+              .setCookieStore(new BlackholeCookieStore())
               .build());
+    }
+
     return client;
   }
 
@@ -24,4 +31,33 @@ public final class GetClient {
       tcPass(client::close);
     client = null;
   }
+
+  private static class BlackholeCookieStore implements CookieStore {
+
+    private final static BlackholeCookieStore BLACK_HOLE =
+        new BlackholeCookieStore();
+
+    @Override
+    public void add(Uri uri, Cookie cookie) {}
+
+    @Override
+    public List<Cookie> get(Uri uri) {
+      return Collections.emptyList();
+    }
+
+    @Override
+    public List<Cookie> getAll() {
+      return Collections.emptyList();
+    }
+
+    @Override
+    public boolean remove(Predicate<Cookie> predicate) {
+      return false;
+    }
+
+    @Override
+    public boolean clear() {
+      return true;
+    }
+  };
 }

1630fb740110dee23ab08aeb93f24b0ed8953156
Albert Liu <albertymliu@gmail.com>
2022-02-05 19:33:24 -0500

    remove simplebatchedfutureengine from another thing

--- src/main/java/scraping/ScrapeSchedge.java
+++ src/main/java/scraping/ScrapeSchedge.java
@@ -21,48 +21,21 @@ public final class ScrapeSchedge {
   private static final String SCHEDGE_URL = "https://schedge.a1liu.com/";
 
   public static List<List<Course>> scrapeFromSchedge(Term term) {
-    BiFunction<Subject, Integer, Future<String>> func = (subject, idx) -> {
-      String school = subject.schoolCode;
-      String major = subject.code.split("-")[0];
-
-      // @TODO Fix this hack to work around weird behavior from V1 and NYU
-      if (school.contentEquals("UI")) {
-        school = "SHU";
-      }
-
-      String[] components =
-          new String[] {"" + term.year, term.semString(), school, major};
-
-      Uri uri =
-          Uri.create(SCHEDGE_URL + String.join("/", components) + "?full=true");
-
-      Request request = new RequestBuilder().setUri(uri).build();
-
-      long start = System.nanoTime();
-
-      return Client.send(request, (resp, throwable) -> {
-        long end = System.nanoTime();
-        double duration = (end - start) / 1000000000.0;
-        logger.info("Fetching took {} seconds: subject={}", duration, subject);
-
-        if (resp == null) {
-          logger.error("Error (subject={}): {}", subject,
-                       throwable.getMessage());
-
-          return null;
-        }
-
-        String text = resp.getResponseBody();
-        return text;
-      });
-    };
-
     Iterator<Subject> subjects = Subject.allSubjects().listIterator();
-    SimpleBatchedFutureEngine<Subject, String> engine =
-        new SimpleBatchedFutureEngine<>(subjects, 20, func);
+
+    FutureEngine<String> engine = new FutureEngine<>();
+    for (int i = 0; i < 20; i++) {
+      if (subjects.hasNext()) {
+        engine.add(getData(term, subjects.next()));
+      }
+    }
 
     ArrayList<List<Course>> output = new ArrayList<>();
     for (String text : engine) {
+      if (subjects.hasNext()) {
+        engine.add(getData(term, subjects.next()));
+      }
+
       if (text == null) {
         continue;
       }
@@ -72,5 +45,40 @@ public final class ScrapeSchedge {
     }
 
     return output;
+  }
+
+  private static Future<String> getData(Term term, Subject subject) {
+    String school = subject.schoolCode;
+    String major = subject.code.split("-")[0];
+
+    // @TODO Fix this hack to work around weird behavior from V1 and NYU
+    if (school.contentEquals("UI")) {
+      school = "SHU";
+    }
+
+    String[] components =
+        new String[] {"" + term.year, term.semString(), school, major};
+
+    Uri uri =
+        Uri.create(SCHEDGE_URL + String.join("/", components) + "?full=true");
+
+    Request request = new RequestBuilder().setUri(uri).build();
+
+    long start = System.nanoTime();
+
+    return Client.send(request, (resp, throwable) -> {
+      long end = System.nanoTime();
+      double duration = (end - start) / 1000000000.0;
+      logger.info("Fetching took {} seconds: subject={}", duration, subject);
+
+      if (resp == null) {
+        logger.error("Error (subject={}): {}", subject, throwable.getMessage());
+
+        return null;
+      }
+
+      String text = resp.getResponseBody();
+      return text;
+    });
   }
 }

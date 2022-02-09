5ff4701955f7821ed71a66e9eb2e77dedd22b7f5
Albert Liu <albertymliu@gmail.com>
2022-02-09 00:51:58 -0500

    idk man

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,32 +8,6 @@ import cx from "classnames";
 import Image from "next/image";
 import Head from "next/head";
 
-/*
-const BootstrapInput = styled.div`
-  border-radius: 4px;
-  border: 1px solid #9e9e9e;
-  font-size: 1rem;
-  padding: 10px 26px 10px 12px;
-  font-weight: bold;
-  color: var(--grey800);
-  font-family: var(--primaryFont);
-  transition: border-color 300ms, box-shadow 300ms;
-
-  &:focus {
-    border-radius: 4px;
-  }
-`;
-
-const StyledImage = styled.img`
-  height: 2.8rem;
-  width: 2.8rem;
-  margin: 1.2vmin;
-  float: right;
-  padding: 0.5rem;
-  border-radius: 100%;
-`;
-*/
-
 const queryClient = new QueryClient();
 
 function App({ Component, pageProps }) {
@@ -47,19 +21,19 @@ function App({ Component, pageProps }) {
   const options = [
     {
       name: "January 2021",
-      code: "ja-2021",
+      code: "ja2021",
     },
     {
       name: "Spring 2021",
-      code: "sp-2021",
+      code: "sp2021",
     },
     {
       name: "Summer 2021",
-      code: "su-2021",
+      code: "su2021",
     },
     {
       name: "Fall 2021",
-      code: "fa-2021",
+      code: "fa2021",
     },
   ];
 
@@ -70,15 +44,14 @@ function App({ Component, pageProps }) {
 
   // if we start on schedule page, the first toggle brings us to home
   // otherwise, the first toggle brings us to schedule page
-  const [toggle, setToggle] = React.useState(
-    getPath() === "/schedule" ? "/" : "/schedule"
+  const [showSchedule, setShowSchedule] = React.useState(
+    getPath() === "/schedule"
   );
-
-  const [showSchedule, setShowSchedule] = React.useState(false);
 
   const handleOnChange = (event) => {
     const code = event.target.value;
-    const [sem, currYear] = code.split("-");
+    const sem = code.substring(0, 2);
+    const currYear = code.substring(2);
 
     setSemester(sem);
     setYear(parseInt(currYear));
@@ -92,26 +65,23 @@ function App({ Component, pageProps }) {
             <a className={headerStyles.title}>Bobcat Search</a>
           </Link>
 
-          <div>Select</div>
-        </div>
+          <div>
+            <div className={headerStyles.selectBox} onClick={() => {}}>
+              <div>Spring 2021</div>
 
-        {/*
-            <Select
-              displayEmpty
-              onChange={handleOnChange}
-              defaultValue={`${semester}-${year}`}
-              value={`${semester}-${year}`}
-              input={<BootstrapInput />}
-            >
-              {options.map((item) => {
-                return (
-                  <MenuItem key={item.name} value={item.code}>
-                    {item.name}
-                  </MenuItem>
-                );
-              })}
-            </Select>
-            */}
+              <svg
+                className={headerStyles.selectArrow}
+                focusable="false"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M7 10l5 5 5-5z"></path>
+              </svg>
+            </div>
+
+            <div></div>
+          </div>
+        </div>
 
         <button
           className={headerStyles.scheduleButton}

887789e87fa6f9557e6c77fee50e497609b590ad
Albert Liu <albertymliu@gmail.com>
2022-09-26 15:26:31 -0400

    Refactored Endpoint: reduce nesting, support custom logic (#115)

--- src/main/java/api/v1/Health.java
+++ src/main/java/api/v1/Health.java
@@ -1,6 +1,7 @@
 package api.v1;
 
 import api.*;
+import io.javalin.http.Context;
 import io.javalin.http.Handler;
 import io.javalin.plugin.openapi.dsl.OpenApiDocumentation;
 
@@ -19,13 +20,11 @@ public final class Health extends App.Endpoint {
                    openApiParam -> { openApiParam.description("OK."); });
   }
 
-  public Handler getHandler() {
-    return ctx -> {
-      // @TODO return statistics on application health here.
+  public Object handleEndpoint(Context ctx) {
+    // @TODO return statistics on application health here.
 
-      // https://stackoverflow.com/questions/17374743/how-can-i-get-the-memory-that-my-java-program-uses-via-javas-runtime-api
+    // https://stackoverflow.com/questions/17374743/how-can-i-get-the-memory-that-my-java-program-uses-via-javas-runtime-api
 
-      ctx.json(1);
-    };
+    return 1;
   }
 }

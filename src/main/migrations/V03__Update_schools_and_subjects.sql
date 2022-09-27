3fb3d68c8f6a10c09e7a9d63696ebb6e421ff089
Albert Liu <albertymliu@gmail.com>
2022-09-27 02:12:52 -0400

    idk did some more ig

--- src/main/migrations/V03__Update_schools_and_subjects.sql
+++ src/main/migrations/V03__Update_schools_and_subjects.sql
@@ -3,6 +3,6 @@ ALTER TABLE schools DROP COLUMN code;
 ALTER TABLE subjects DROP COLUMN school;
 
 ALTER TABLE subjects
-  ADD COLUMN school integer
+  ADD COLUMN school integer NOT NULL
   REFERENCES schools(id)
   ON DELETE CASCADE;

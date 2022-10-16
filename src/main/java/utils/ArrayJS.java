aa63e281e70e7f72a844ff508482032b9ee671b2
Albert Liu <albertymliu@gmail.com>
2022-10-16 15:42:42 -0400

    meh

---
+++ src/main/java/utils/ArrayJS.java
@@ -0,0 +1,32 @@
+package utils;
+
+import java.util.function.*;
+
+public final class ArrayJS {
+  public interface ArrayFunc2<T> {
+    boolean run(T obj, int index);
+  }
+
+  public interface ArrayFunc<T> extends ArrayFunc2<T> {
+    boolean run(T obj);
+
+    default boolean run(T obj, int index) { return this.run(obj); }
+  }
+
+  public static <T> T find(Iterable<T> iterable, ArrayFunc<T> func) {
+    return find(iterable, (ArrayFunc2<T>)func);
+  }
+
+  public static <T> T find(Iterable<T> iterable, ArrayFunc2<T> func) {
+    int i = 0;
+    for (var t : iterable) {
+      if (func.run(t, i)) {
+        return t;
+      }
+
+      i++;
+    }
+
+    return null;
+  }
+}

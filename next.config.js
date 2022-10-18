6f7fb735e3ec8871998645cec33164d7a6ec7d6d
Albert Liu <albertymliu@gmail.com>
2022-10-18 00:43:15 -0400

    meh

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   trailingSlash: true,
 
+  // @NOTE: These aren't applied when exporting, but
+  // that's fine. It doesn't matter.
+  //            - Albert Liu, Oct 18, 2022 Tue 00:42
   async rewrites() {
     return [
       {

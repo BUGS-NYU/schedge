5fd4aa2023572bcb0355325acdd969aa768993f1
Albert Liu <albertymliu@gmail.com>
2022-11-19 16:09:51 -0500

    meh

--- src/main/java/actions/ScrapeTerm.java
+++ src/main/java/actions/ScrapeTerm.java
@@ -5,34 +5,36 @@ import static database.UpdateSchools.*;
 import static utils.Nyu.*;
 
 import database.GetConnection;
+import java.sql.*;
 import me.tongfei.progressbar.ProgressBar;
 import scraping.PeopleSoftClassSearch;
 
 public class ScrapeTerm {
 
   public static void scrapeTerm(Term term, boolean display) {
-    if (!display) {
-      scrapeTerm(term, null);
-      return;
-    }
+    GetConnection.withConnection(conn -> {
+      if (!display) {
+        scrapeTerm(conn, term, null);
+        return;
+      }
 
-    try (ProgressBar bar = new ProgressBar("Scrape " + term.json(), -1)) {
-      scrapeTerm(term, bar);
-    }
+      try (ProgressBar bar = new ProgressBar("Scrape " + term.json(), -1)) {
+        scrapeTerm(conn, term, bar);
+      }
+    });
   }
 
   // @Note: bar is nullable
-  static void scrapeTerm(Term term, ProgressBar bar) {
-    GetConnection.withConnection(conn -> {
-      clearPrevious(conn, term);
+  static void scrapeTerm(Connection conn, Term term, ProgressBar bar)
+      throws SQLException {
+    clearPrevious(conn, term);
 
-      var termData = PeopleSoftClassSearch.scrapeTerm(term, bar);
-      updateSchoolsForTerm(conn, term, termData.getSchools());
+    var termData = PeopleSoftClassSearch.scrapeTerm(term, bar);
+    updateSchoolsForTerm(conn, term, termData.getSchools());
 
-      while (termData.hasNext()) {
-        var coursesBatch = termData.next();
-        insertCourses(conn, term, coursesBatch);
-      }
-    });
+    while (termData.hasNext()) {
+      var coursesBatch = termData.next();
+      insertCourses(conn, term, coursesBatch);
+    }
   }
 }

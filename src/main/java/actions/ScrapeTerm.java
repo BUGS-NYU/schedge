b6c5892a5bc43d5a68b3e4b4314d3336cb374da1
Albert Liu <albertymliu@gmail.com>
2022-11-27 02:29:52 -0500

    mhe

--- src/main/java/actions/ScrapeTerm.java
+++ src/main/java/actions/ScrapeTerm.java
@@ -3,8 +3,8 @@ package actions;
 import static database.InsertCourses.*;
 import static database.UpdateSchools.*;
 import static scraping.PeopleSoftClassSearch.*;
-
 import static utils.Nyu.*;
+
 import java.sql.*;
 import java.util.function.*;
 import scraping.PeopleSoftClassSearch;
@@ -12,7 +12,8 @@ import scraping.PeopleSoftClassSearch;
 public class ScrapeTerm {
 
   // @Note: bar is nullable
-  public static void scrapeTerm(Connection conn, Term term, Consumer<ScrapeEvent> consumer)
+  public static void scrapeTerm(Connection conn, Term term,
+                                Consumer<ScrapeEvent> consumer)
       throws SQLException {
     clearPrevious(conn, term);
     var termData = PeopleSoftClassSearch.scrapeTerm(term, consumer);

e61228b426a6e1bccb75749f5b716147af580a37
Albert Liu <albertymliu@gmail.com>
2022-11-27 01:17:39 -0500

    meh

--- src/test/java/test/ParserTest.java
+++ src/test/java/test/ParserTest.java
@@ -1,12 +1,9 @@
 package test;
 
 import static actions.CopyTermFromProduction.*;
-import static utils.JsonMapper.*;
 import static utils.Nyu.*;
 import static utils.Try.*;
 
-import api.*;
-import io.javalin.testtools.JavalinTest;
 import org.junit.*;
 import org.slf4j.*;
 import scraping.PSCoursesParser;
@@ -16,9 +13,6 @@ public class ParserTest {
   static Logger logger = LoggerFactory.getLogger("test.ParserTest");
   @Test
   public void testParserBasic() {
-    // sp2021
-    // CSCI-UA
-
     String html = Utils.readResource("/csci-ua-sp2021.html");
     var ctx = Ctx(logger);
     PSCoursesParser.parseSubject(ctx, html, "CSCI-UA", e -> {

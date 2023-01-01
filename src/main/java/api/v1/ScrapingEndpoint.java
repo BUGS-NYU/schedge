a1f7d0ed9ef3f5556a4a172c2e38a427ee5d8320
Albert Liu <albertymliu@gmail.com>
2023-01-01 17:23:26 -0500

    meh

--- src/main/java/api/v1/ScrapingEndpoint.java
+++ src/main/java/api/v1/ScrapingEndpoint.java
@@ -75,17 +75,21 @@ public final class ScrapingEndpoint {
 
       var bar = builder.build();
 
-      ScrapeJob job = switch (source) {
-        case "schedge-v1" -> ScrapeSchedgeV1::scrapeFromSchedge;
-        case "sis.nyu.edu" -> PSClassSearch::scrapeTerm;
-        default -> {
-          var warning = "Invalid source: " + source;
-          ctx.send(warning);
-          logger.warn(warning);
+      ScrapeJob job = run(() -> {
+        switch (source) {
+        case "schedge-v1":
+          return ScrapeSchedgeV1::scrapeFromSchedge;
+        case "sis.nyu.edu":
+          return PSClassSearch::scrapeTerm;
+          default -> {
+            var warning = "Invalid source: " + source;
+            ctx.send(warning);
+            logger.warn(warning);
 
-          yield null;
+            return null;
+          }
         }
-      };
+      });
 
       if (job == null) {
         return "No job to run!";
@@ -129,90 +133,89 @@ public final class ScrapingEndpoint {
         }
 
         var authString = ctx.header("Authorization");
-        authString = authString != null ? authString :
-          "";
+        authString = authString != null ? authString : "";
 
-          var digest = tcPass(() -> MessageDigest.getInstance("SHA-256"));
-          var authBytes = authString.getBytes(StandardCharsets.UTF_8);
-          var authHash = digest.digest(authBytes);
+        var digest = tcPass(() -> MessageDigest.getInstance("SHA-256"));
+        var authBytes = authString.getBytes(StandardCharsets.UTF_8);
+        var authHash = digest.digest(authBytes);
 
-          if (!Arrays.equals(authHash, AUTH_HASH) ||
-              !authString.equals(AUTH_STRING)) {
-            ctx.closeSession(1000, "Failed: Unauthorized");
-            return;
-          }
+        if (!Arrays.equals(authHash, AUTH_HASH) ||
+            !authString.equals(AUTH_STRING)) {
+          ctx.closeSession(1000, "Failed: Unauthorized");
+          return;
+        }
 
-          var id = new Object();
-          ctx.attribute("identity", id);
+        var id = new Object();
+        ctx.attribute("identity", id);
 
-          if (!MUTEX.compareAndSet(null, id)) {
-            ctx.closeSession(1000, "Already running a scraping job!");
-            return;
-          }
+        if (!MUTEX.compareAndSet(null, id)) {
+          ctx.closeSession(1000, "Already running a scraping job!");
+          return;
+        }
 
-          var task = new FutureTask<>(() -> scrape(ctx));
-          CURRENT_SCRAPE = task;
+        var task = new FutureTask<>(() -> scrape(ctx));
+        CURRENT_SCRAPE = task;
 
-          CompletableFuture.runAsync(() -> {
-            var closeCode = 1011;
-            var closeReason = "Unknown reason";
+        CompletableFuture.runAsync(() -> {
+          var closeCode = 1011;
+          var closeReason = "Unknown reason";
 
-            try {
-              task.run();
-              closeReason = task.get();
+          try {
+            task.run();
+            closeReason = task.get();
 
-              if (closeReason.equals("Done!"))
-                closeCode = 1000;
-            } catch (CancellationException
-                         e) { // Do nothing because job was cancelled
-              closeReason = "Job was cancelled";
-              logger.info(closeReason);
-            } catch (InterruptedException
-                         e) { // Do nothing because job was interrupted
-              closeReason = "Job was interrupted";
-              logger.info(closeReason);
-            } catch (ExecutionException e) {
-              closeReason = "Job failed: " + e.getMessage();
-              logger.error(closeReason, e);
-            } finally {
-              var newLock = new Object();
-              if (!MUTEX.compareAndSet(id, newLock)) {
-                return;
-              }
-
-              CURRENT_SCRAPE = null;
-              ctx.attribute("identity", null);
-              ctx.send(closeReason);
-              ctx.closeSession(closeCode, closeReason);
-
-              if (!MUTEX.compareAndSet(newLock, null)) {
-                logger.warn("Failed to unlock mutex");
-              }
+            if (closeReason.equals("Done!"))
+              closeCode = 1000;
+          } catch (
+              CancellationException e) { // Do nothing because job was cancelled
+            closeReason = "Job was cancelled";
+            logger.info(closeReason);
+          } catch (InterruptedException
+                       e) { // Do nothing because job was interrupted
+            closeReason = "Job was interrupted";
+            logger.info(closeReason);
+          } catch (ExecutionException e) {
+            closeReason = "Job failed: " + e.getMessage();
+            logger.error(closeReason, e);
+          } finally {
+            var newLock = new Object();
+            if (!MUTEX.compareAndSet(id, newLock)) {
+              return;
             }
-          });
-        });
 
-        ws.onMessage(ctx -> {});
+            CURRENT_SCRAPE = null;
+            ctx.attribute("identity", null);
+            ctx.send(closeReason);
+            ctx.closeSession(closeCode, closeReason);
 
-        ws.onClose(ctx -> {
-          var id = ctx.attribute("identity");
-          if (id == null)
-            return;
-
-          logger.info("Closing scrape early");
-
-          var newLock = new Object();
-          if (!MUTEX.compareAndSet(id, newLock)) {
-            return;
-          }
-
-          CURRENT_SCRAPE.cancel(true);
-          CURRENT_SCRAPE = null;
-
-          if (!MUTEX.compareAndSet(newLock, null)) {
-            logger.warn("Failed to unlock mutex");
+            if (!MUTEX.compareAndSet(newLock, null)) {
+              logger.warn("Failed to unlock mutex");
+            }
           }
         });
+      });
+
+      ws.onMessage(ctx -> {});
+
+      ws.onClose(ctx -> {
+        var id = ctx.attribute("identity");
+        if (id == null)
+          return;
+
+        logger.info("Closing scrape early");
+
+        var newLock = new Object();
+        if (!MUTEX.compareAndSet(id, newLock)) {
+          return;
+        }
+
+        CURRENT_SCRAPE.cancel(true);
+        CURRENT_SCRAPE = null;
+
+        if (!MUTEX.compareAndSet(newLock, null)) {
+          logger.warn("Failed to unlock mutex");
+        }
+      });
     });
   }
 }
